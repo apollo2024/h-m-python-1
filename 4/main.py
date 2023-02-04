bbfe0935b73d91a1d9a36e58e74ff2687225b3e3
muhammadkodyr <okk972016@gmail.com>
2023-02-04 22:02:10 +0500

    fixed the struggles of code

--- 4/main.py
+++ 4/main.py
@@ -3,12 +3,12 @@
 #  что Петя и Сережа сделали одинаковое количество журавликов,
 #  а Катя сделала в два раза больше журавликов, чем Петя и Сережа вместе?
 
-from random import randint
+from random import randrange
 
-s = randint(6, 999)
-
-pety = s // 3 // 2
-sergey = pety
-katy = (pety + sergey) * 2
-print(f'всего сделано {s}')
-print(f'петя сделал {pety}, сергей сделал {sergey}, катя сделала {katy}')
+s = randrange(6, 1002, 3)
+if s % 2 == 0:
+    pety = s // 6
+    sergey = pety
+    katy = (pety + sergey) * 2
+    print(f'всего сделано {s}')
+    print(f'петя сделал {pety}, сергей сделал {sergey}, катя сделала {katy}')
